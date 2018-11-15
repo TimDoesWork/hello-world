496dfed98af6e75c67fd8f1c2813bb03de9dcd23
Milan <milan.gfeller@gmx.net>
2018-11-15 10:48:40 +0100

    Feature B - New Attribute 'String name' and getter/setter Methods

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,20 @@
 public class HelloWorld{
 	private int i;
+	private String name;
+
 	public HelloWorld(){
 		i = 0;
 	}
 	
 	public int getI(){
 		return i;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
